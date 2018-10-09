261b35c210bae3a73e85cbd5aaee4cdfb724c317
RohiniPunati <43442547+RohiniPunati@users.noreply.github.com>
2018-10-09 10:45:48 +0530

    two numbers task(Day1)

---
+++ new_task1/TwoNumbers.java
@@ -0,0 +1,15 @@
+package newTask1;
+
+public class TwoNumbers {
+public static void main(String[] args) {
+	int a=12,b=14;
+	if((a-b)>=2){
+		System.out.println(a);
+	}
+	else if((b-a)>=2){
+		System.out.println(b);
+	}else{
+		System.out.println("INCONCLUSIVE");
+	}
+}
+}
